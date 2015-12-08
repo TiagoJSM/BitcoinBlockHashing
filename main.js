c4c37ab004efdcf78497ca1c9264d51d69a02276
Tiago <tiago.martins180@gmail.com>
2015-12-08 15:14:10 +0000

    Changed the way to obtain the merkleroot property.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var crypto = require('crypto');
 var http = require('https');
 
+Array.prototype.last = function() {
+    return this[this.length-1];
+}
+
 var defaultHashPrevBlock = "0000000000000000000000000000000000000000000000000000000000000000";
 
 function getBlock(id, callback){
@@ -34,50 +38,46 @@ function doubleHash(data){
 			.digest("hex");
 	return hashed;
 }
-	
+
+function getMerkleRootAux(transactions){
+	if(transactions.length == 1){
+		return transactions[0];
+	}
+	var combinedTransactions = []
+	if(transactions.length % 2 != 0){
+		transactions.push(transactions.last());
+	}
+	console.log(transactions);
+	for(var idx = 0; idx < transactions.length; idx+=2){
+		var buffers = [new Buffer(reverseHexString(transactions[idx]), 'hex'), new Buffer(reverseHexString(transactions[idx + 1]), 'hex')];
+		combinedTransactions.push(reverseHexString(doubleHash(Buffer.concat(buffers))));
+	}
+	return getMerkleRootAux(combinedTransactions);
+}
+
+function getMerkleRoot(genesisBlock){
+	var transactions = genesisBlock.tx.slice();
+	return reverseHexString(getMerkleRootAux(transactions));
+}
+
 function buildBlockHeader(genesisBlock){
 	var buffer = new Buffer(4);
 	buffer.fill(0);
 	buffer.writeUInt32LE(genesisBlock.version);
-	var merkleroot = reverseHexString(genesisBlock.merkleroot);
+	
+	var merkleroot = getMerkleRoot(genesisBlock);	
 	var time = reverseHexString(genesisBlock.time.toString(16));
 	var bits = reverseHexString(genesisBlock.bits);
 	var nonce = reverseHexString(genesisBlock.nonce.toString(16));
 	var hashPrevBlock = typeof genesisBlock.previousblockhash == 'undefined' ? defaultHashPrevBlock : reverseHexString(genesisBlock.previousblockhash);
 	
-	//still not sure about hashPrevBlock
 	return buffer.toString('hex') + hashPrevBlock +	merkleroot + time + bits + nonce;
 }
 	
 function verifyHash(genesisBlock){
-	console.log("");
-	console.log("");
-	var a = new Buffer(reverseHexString(genesisBlock.tx[0]), 'hex');
-	var b = new Buffer(reverseHexString(genesisBlock.tx[1]), 'hex');
-	var buffers = [a, b];
-	//console.log(buffers);
-	//console.log(Buffer.concat(buffers));
-	//console.log(reverseHexString( genesisBlock.tx[0] ) + " " + reverseHexString( genesisBlock.tx[1] ));
-	//console.log(reverseHexString( genesisBlock.tx[0] ) + reverseHexString( genesisBlock.tx[1] ));
-	console.log("");
-	console.log("");
-	console.log(reverseHexString(doubleHash(Buffer.concat(buffers))));
-	
-	/*console.log( 
-		doubleHash( 
-			new Buffer( 
-				reverseHexString(
-					new Buffer( reverseHexString( genesisBlock.tx[0], 'hex' ) ) ) 
-					new Buffer( reverseHexString( genesisBlock.tx[1], 'hex' ) ) ) ), 'hex') ) );*/
-	
-	console.log("");
-	console.log("");
-	
 	var blockHeader = buildBlockHeader(genesisBlock);
 	var buffer = new Buffer(blockHeader, 'hex');
-
 	var data = doubleHash(buffer);
-
 	return reverseHexString(data) == genesisBlock.hash;
 }
 
